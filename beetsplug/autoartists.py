966029f576679941ceba666e773279c4532deeab
Charliec111 <charliecrist4nj@gmail.com>
2025-02-02 15:38:54 -0500

    black reformatting

--- beetsplug/autoartists.py
+++ beetsplug/autoartists.py
@@ -19,7 +19,6 @@ from beets.ui import decargs, print_, should_write
 from beets.plugins import BeetsPlugin
 
 
-
 class AutoArtistsPlugin(BeetsPlugin):
     def __init__(self):
         super().__init__()
@@ -28,16 +27,15 @@ class AutoArtistsPlugin(BeetsPlugin):
                 "auto": False,
                 "overwrite": True,
                 "separators": ["␟", ", ", " & ", " and ", " + ", " with ", "/", ";"],
-                "single_artists": []
+                "single_artists": [],
             }
         )
-        self.item_types = {
-        }
-        # self.single_artist_list is a list of artists who should not be broken up into multiples, 
+        self.item_types = {}
+        # self.single_artist_list is a list of artists who should not be broken up into multiples,
         # see readme for more info
         self.overwrite = self.config["overwrite"]
-        self.single_artist_list = [ x.as_str() for x in self.config["single_artists"] ]
-        self.separators = [ x.as_str() for x in self.config["separators"]]
+        self.single_artist_list = [x.as_str() for x in self.config["single_artists"]]
+        self.separators = [x.as_str() for x in self.config["separators"]]
         if self.config["auto"]:
             self.import_stages = [self.imported]
         self._log.debug(f"Single artist list: {self.single_artist_list}")
@@ -74,21 +72,35 @@ class AutoArtistsPlugin(BeetsPlugin):
         # choices is a list of tuples of (song, artists list) for each match in the query
         choices = []
         if not self.overwrite:
-            query_result_songs = [ x for x in query_result_songs if "artists" not in x or len(x["artists"]) < 1 ]
+            query_result_songs = [
+                x
+                for x in query_result_songs
+                if "artists" not in x or len(x["artists"]) < 1
+            ]
         for song in query_result_songs:
             artist = str(song["artist"])
             title = str(song["title"])
             artists = None if "artists" not in song else song["artists"]
-            artists_result = self.get_artists(artist=artist, title=title,artists=artists)
-            choices.append((song,artists_result))
+            artists_result = self.get_artists(
+                artist=artist, title=title, artists=artists
+            )
+            choices.append((song, artists_result))
 
         old_choices_len = len(choices)
-        new_choices = [ choice for choice in choices if "artists" not in choice[0] or not lists_have_same_strings(choice[0]["artists"], choice[1]) ]
-        unchanged_choices = [ choice for choice in choices if choice not in new_choices ]
+        new_choices = [
+            choice
+            for choice in choices
+            if "artists" not in choice[0]
+            or not lists_have_same_strings(choice[0]["artists"], choice[1])
+        ]
+        unchanged_choices = [choice for choice in choices if choice not in new_choices]
         choices = new_choices
         overwrite_message = ""
-        if not self.overwrite: overwrite_message =" that would not be overwritten"
-        print_(f"{old_choices_len} found in query{overwrite_message}, {old_choices_len - len(choices)} had no changes")
+        if not self.overwrite:
+            overwrite_message = " that would not be overwritten"
+        print_(
+            f"{old_choices_len} found in query{overwrite_message}, {old_choices_len - len(choices)} had no changes"
+        )
         if len(choices) == 0:
             if old_choices_len > 0:
                 print_("Nothing to change")
@@ -100,18 +112,18 @@ class AutoArtistsPlugin(BeetsPlugin):
         for choice in unchanged_choices:
             song = choice[0]
             final_list = choice[1]
-            artists=[ ]
+            artists = []
             if "artists" in song:
-                artists=song["artists"]
+                artists = song["artists"]
             print_(f"{song}: {artists}")
         print_("---")
         print_("Changes:")
         for choice in choices:
             song = choice[0]
             final_list = choice[1]
-            artists=[  ]
+            artists = []
             if "artists" in song:
-                artists=song["artists"]
+                artists = song["artists"]
             print_(f"{song}: ")
             print_(f"old: {artists} => new: {final_list}")
 
@@ -130,24 +142,27 @@ class AutoArtistsPlugin(BeetsPlugin):
         for choice in choices:
             song = choice[0]
             final_list = choice[1]
-            artists=[ "" ]
+            artists = [""]
             if "artists" in song:
-                artists=song["artists"]
+                artists = song["artists"]
             if keep_asking:
                 print_(f"---")
                 print_(f"{song}: " + str(final_list))
                 confirm_item = input(
                     f"Changing from {artists} => new: {final_list}. Confirm? (y/n)\n"
                 ).lower()
-            if not lists_have_same_strings(artists, final_list) and (not keep_asking or confirm_item in ["y", "yes"]):
+            if not lists_have_same_strings(artists, final_list) and (
+                not keep_asking or confirm_item in ["y", "yes"]
+            ):
                 song["artists"] = final_list
                 if should_write():
                     song.try_write()
                 song.store()
-    # input: artist and title are strings from the song, artists is the list of 
+
+    # input: artist and title are strings from the song, artists is the list of
     # strings of artist names the song already has or None
     # output: returns a list of strings of artists correspoding to the artist/title/artists
-    def get_artists(self,artist, title, artists=None):
+    def get_artists(self, artist, title, artists=None):
         auto_artists = []
         if artists:
             auto_artists = auto_artists + artists
@@ -156,9 +171,9 @@ class AutoArtistsPlugin(BeetsPlugin):
                 self._log.debug(f"single artist {single_artist}: {artist}")
                 if single_artist not in auto_artists:
                     auto_artists = auto_artists + [single_artist]
-                artist = re.sub(re.escape(single_artist),"",artist, re.IGNORECASE)
+                artist = re.sub(re.escape(single_artist), "", artist, re.IGNORECASE)
                 self._log.debug(f"artist is now {artist}")
-        for i in [ "(feat. ", "(featuring ", "(with " ]:
+        for i in ["(feat. ", "(featuring ", "(with "]:
             if i in artist.lower():
                 artist = re.sub(
                     r"(.*) *[\(\[](feat\.|with|featuring) ([^)\]]*)[\)\]].*$",
@@ -169,14 +184,14 @@ class AutoArtistsPlugin(BeetsPlugin):
         for i in [" feat. ", " featuring ", " Feat. ", " Featuring "]:
             if i in artist:
                 artist = artist.replace(i, ", ")
-        for new_artist in split_artists_string(artist,self.single_artist_list, separators=self.separators):
+        for new_artist in split_artists_string(
+            artist, self.single_artist_list, separators=self.separators
+        ):
             if new_artist not in auto_artists:
                 self._log.debug(f"Adding artist {new_artist}")
                 auto_artists.append(new_artist)
-        if re.match (
-            r".* [\(\[](feat\.?|with|featuring) ([^)\]]*)[\)\]].*",
-            title,
-            re.IGNORECASE
+        if re.match(
+            r".* [\(\[](feat\.?|with|featuring) ([^)\]]*)[\)\]].*", title, re.IGNORECASE
         ):
             featured_artist_string = re.sub(
                 r".*[\(\[](feat\.?|with|featuring) ([^)\]]*)[\)\]].*$",
@@ -184,12 +199,18 @@ class AutoArtistsPlugin(BeetsPlugin):
                 title,
                 flags=re.IGNORECASE,
             )
-            featured_artists = split_artists_string(featured_artist_string, self.single_artist_list, separators=self.separators)
-            auto_artists = auto_artists + [ x for x in featured_artists if x not in auto_artists ]
+            featured_artists = split_artists_string(
+                featured_artist_string,
+                self.single_artist_list,
+                separators=self.separators,
+            )
+            auto_artists = auto_artists + [
+                x for x in featured_artists if x not in auto_artists
+            ]
 
-        auto_artists = [ x.strip() for x in auto_artists ]
+        auto_artists = [x.strip() for x in auto_artists]
 
-        #final cleanups
+        # final cleanups
         final_list = []
         normalized_artists_strings = []
         for auto_artist in auto_artists:
@@ -203,29 +224,38 @@ class AutoArtistsPlugin(BeetsPlugin):
                 normalized_artists_strings.append(normalized_artist)
                 # Put the track artist first, otherwise add at the end
                 if auto_artist == artist:
-                    final_list.insert(0,auto_artist)
+                    final_list.insert(0, auto_artist)
                 else:
                     final_list.append(auto_artist)
         return final_list
+
     def imported(self, session, task):
         for song in task.imported_items():
             artist = str(song["artist"])
             title = str(song["title"])
             existing_artists = None if "artists" not in song else song["artists"]
-            artists_result = self.get_artists(artist=artist, title=title,artists=existing_artists)
+            artists_result = self.get_artists(
+                artist=artist, title=title, artists=existing_artists
+            )
             self._log.info(f"Autoartists: {song} has artists {existing_artists}")
-            if not lists_have_same_strings(existing_artists, artists_result): 
+            if not lists_have_same_strings(existing_artists, artists_result):
                 song["artists"] = artists_result
                 if should_write():
                     song.try_write()
                 song.store()
                 self._log.info(f"Autoartists: Added artists {artists_result} to {song}")
+
+
 # Input: artists_string is a string with one or multiple artists
 # single_artists is a list of strings which should be treated as one artist (do not separate them)
 # Output: A list of strings of the artist names in artists_string
-def split_artists_string(artists_string,single_artists, separators=["␟", ", ", " & ", " and ", " + ", " with ", "/", ";"]):
-    separator=separators[0]
-    other_separators=separators[1:]
+def split_artists_string(
+    artists_string,
+    single_artists,
+    separators=["␟", ", ", " & ", " and ", " + ", " with ", "/", ";"],
+):
+    separator = separators[0]
+    other_separators = separators[1:]
     artists = []
     for artist in single_artists:
         if artist.lower() in artists_string.lower():
@@ -234,20 +264,30 @@ def split_artists_string(artists_string,single_artists, separators=["␟", ", ",
                 artists_string = ""
                 return artists
             else:
-                artists_string = re.sub(re.escape(artist),"",artists_string, re.IGNORECASE)
+                artists_string = re.sub(
+                    re.escape(artist), "", artists_string, re.IGNORECASE
+                )
     for i in other_separators:
         artists_string = artists_string.replace(i, separator)
-    artists = [ x for x in (artists_string.split(separator) + artists) if x not in ["", " "]]
+    artists = [
+        x for x in (artists_string.split(separator) + artists) if x not in ["", " "]
+    ]
     return artists
+
 
 # a,b are lists of strings
 # return True if every string in a is in b and vice versa, False otherwise
-def lists_have_same_strings(a,b):
-    if len(a) != len(b): return False
-    lower_a = [ normalize_string(x) for x in a ]
-    lower_b = [ normalize_string(x) for x in b ]
-    diff = [ x for x in lower_a if x not in lower_b ] + [ x for x in lower_b if x not in lower_a ]
+def lists_have_same_strings(a, b):
+    if len(a) != len(b):
+        return False
+    lower_a = [normalize_string(x) for x in a]
+    lower_b = [normalize_string(x) for x in b]
+    diff = [x for x in lower_a if x not in lower_b] + [
+        x for x in lower_b if x not in lower_a
+    ]
     return not len(diff) > 0
+
+
 # This function is mostly copied from a project called maloja by krateng
 # most of it is unnecessary, it's copied and pasted from my uses of it in
 # other projects where song artists/tracks strings are normalized and compared
--- test/plugins/test_autoartists.py
+++ test/plugins/test_autoartists.py
@@ -5,37 +5,72 @@ from beets.test import _common
 from beets.test.helper import BeetsTestCase
 from beetsplug import autoartists
 
+
 class AutoArtistsPluginTest(BeetsTestCase):
     def setUp(self):
         super().setUp()
         self.plugin = autoartists.AutoArtistsPlugin()
-    def _setup_config(
-        self, single_artists=["Earth, Wind & Fire", "A and B"]
-    ):
+
+    def _setup_config(self, single_artists=["Earth, Wind & Fire", "A and B"]):
         print(f"single artists: {single_artists}")
         self.config["autoartists"]["single_artists"] = single_artists
         self.plugin.__init__()
+
     def test_whitelist(self):
         self.setUp()
         artist_whitelist = ["Earth, Wind & Fire"]
         self._setup_config(single_artists=artist_whitelist)
         test_result = self.plugin.get_artists("Jim Croce", "a song title")
-        assert test_result == ['Jim Croce']
+        assert test_result == ["Jim Croce"]
 
-
-        assert len(self.plugin.get_artists("Jim Croce and Another artist", "a song title")) == 2 
-        assert len(self.plugin.get_artists("Jim Croce & Another artist", "a song title")) == 2
-        assert len(self.plugin.get_artists(artist_whitelist[0], "September")) == 1 
-        assert len(self.plugin.get_artists(artist_whitelist[0], "September")) == 1 
-        assert len(self.plugin.get_artists("An Artist", f"September (feat. {artist_whitelist[0]})")) == 2 
-        assert len(self.plugin.get_artists("An Artist", f"September (featuring {artist_whitelist[0]})")) == 2 
-        assert len(self.plugin.get_artists("An Artist", f"September (with {artist_whitelist[0]})")) == 2 
+        assert (
+            len(self.plugin.get_artists("Jim Croce and Another artist", "a song title"))
+            == 2
+        )
+        assert (
+            len(self.plugin.get_artists("Jim Croce & Another artist", "a song title"))
+            == 2
+        )
+        assert len(self.plugin.get_artists(artist_whitelist[0], "September")) == 1
+        assert len(self.plugin.get_artists(artist_whitelist[0], "September")) == 1
+        assert (
+            len(
+                self.plugin.get_artists(
+                    "An Artist", f"September (feat. {artist_whitelist[0]})"
+                )
+            )
+            == 2
+        )
+        assert (
+            len(
+                self.plugin.get_artists(
+                    "An Artist", f"September (featuring {artist_whitelist[0]})"
+                )
+            )
+            == 2
+        )
+        assert (
+            len(
+                self.plugin.get_artists(
+                    "An Artist", f"September (with {artist_whitelist[0]})"
+                )
+            )
+            == 2
+        )
 
         artist_whitelist = ["Jim with Bob", "Earth, Wind & Fire"]
         self._setup_config(single_artists=artist_whitelist)
-        assert len(self.plugin.get_artists(artist_whitelist[0], "September")) == 1 
+        assert len(self.plugin.get_artists(artist_whitelist[0], "September")) == 1
 
-        artist_whitelist = ["AC/DC", "Daryl Hall & John Oates", "Earth, Wind & Fire", "Simon & Garfunkel", "Prince and The Revolution", "Florence + the Machine", "A with B"]
+        artist_whitelist = [
+            "AC/DC",
+            "Daryl Hall & John Oates",
+            "Earth, Wind & Fire",
+            "Simon & Garfunkel",
+            "Prince and The Revolution",
+            "Florence + the Machine",
+            "A with B",
+        ]
         self._setup_config(single_artists=artist_whitelist)
         for i in artist_whitelist:
             test_result = self.plugin.get_artists(i, "Song Title")
@@ -44,7 +79,9 @@ class AutoArtistsPluginTest(BeetsTestCase):
             except AssertionError as e:
                 print(f"Failed on test result: {test_result}")
                 raise e
-            test_result = self.plugin.get_artists(f"{i} feat. Another Artist", "Song Title")
+            test_result = self.plugin.get_artists(
+                f"{i} feat. Another Artist", "Song Title"
+            )
             try:
                 assert test_result == [i, "Another Artist"]
             except AssertionError as e:
@@ -63,60 +100,84 @@ class AutoArtistsPluginTest(BeetsTestCase):
             # Test with 2 featured artists
             for separator in [", ", " & ", " + ", "/"]:
                 for test_result in [
-                    self.plugin.get_artists(f"B", f"Song Title (feat. {i}{separator}OtherArtist)"),
-                    self.plugin.get_artists(f"B", f"Song Title (featuring {i}{separator}OtherArtist)"),
-                    self.plugin.get_artists(f"B", f"Song Title (with {i}{separator}OtherArtist)"),
+                    self.plugin.get_artists(
+                        f"B", f"Song Title (feat. {i}{separator}OtherArtist)"
+                    ),
+                    self.plugin.get_artists(
+                        f"B", f"Song Title (featuring {i}{separator}OtherArtist)"
+                    ),
+                    self.plugin.get_artists(
+                        f"B", f"Song Title (with {i}{separator}OtherArtist)"
+                    ),
                 ]:
                     try:
-                        assert test_result in [ ["B", i, "OtherArtist"],["B", "OtherArtist", i] ]
+                        assert test_result in [
+                            ["B", i, "OtherArtist"],
+                            ["B", "OtherArtist", i],
+                        ]
                     except AssertionError as e:
                         print(f"Failed on test result: {test_result}")
                         raise e
             # Test with 3 featured artists
             for separator in [", ", " & ", " + "]:
                 for test_result in [
-                    self.plugin.get_artists(f"B", f"Song Title (feat. {i}, AnArtist{separator}OtherArtist)"),
-                    self.plugin.get_artists(f"B", f"Song Title (featuring {i}, AnArtist{separator}OtherArtist)"),
-                    self.plugin.get_artists(f"B", f"Song Title (with {i}, AnArtist{separator}OtherArtist)"),
+                    self.plugin.get_artists(
+                        f"B", f"Song Title (feat. {i}, AnArtist{separator}OtherArtist)"
+                    ),
+                    self.plugin.get_artists(
+                        f"B",
+                        f"Song Title (featuring {i}, AnArtist{separator}OtherArtist)",
+                    ),
+                    self.plugin.get_artists(
+                        f"B", f"Song Title (with {i}, AnArtist{separator}OtherArtist)"
+                    ),
                 ]:
                     try:
-                        assert sorted(test_result) == sorted(["B",i, "AnArtist","OtherArtist"])
+                        assert sorted(test_result) == sorted(
+                            ["B", i, "AnArtist", "OtherArtist"]
+                        )
                     except AssertionError as e:
                         print(f"Failed on test result: {sorted(test_result)}")
                         raise e
+
     def test_separating_artist(self):
         self.setUp()
         artist_whitelist = ["Earth, Wind & Fire"]
         self._setup_config(single_artists=artist_whitelist)
-        #Beyoncé feat. JAY-Z & Kanye West - BEYONCÉ (platinum edition) - Drunk in Love (remix) 
-        test_result = self.plugin.get_artists(artist="Beyoncé feat. JAY-Z & Kanye West", title="Drunk in Love (remix)")
-        assert test_result == ['Beyoncé', 'JAY-Z', 'Kanye West']
-        for test in [ 
-                ("Jim Croce and Another artist", "a song title"), 
-                ("Jim Croce & Another artist", "a song title"),
-                ("Jim Croce with Another artist", "a song title"), 
-                (f"Jim Croce feat. Another artist", "a song title"), 
-                ]:
+        # Beyoncé feat. JAY-Z & Kanye West - BEYONCÉ (platinum edition) - Drunk in Love (remix)
+        test_result = self.plugin.get_artists(
+            artist="Beyoncé feat. JAY-Z & Kanye West", title="Drunk in Love (remix)"
+        )
+        assert test_result == ["Beyoncé", "JAY-Z", "Kanye West"]
+        for test in [
+            ("Jim Croce and Another artist", "a song title"),
+            ("Jim Croce & Another artist", "a song title"),
+            ("Jim Croce with Another artist", "a song title"),
+            (f"Jim Croce feat. Another artist", "a song title"),
+        ]:
             test_result = self.plugin.get_artists(test[0], test[1])
             try:
-                assert test_result == ['Jim Croce', 'Another artist']
+                assert test_result == ["Jim Croce", "Another artist"]
             except AssertionError as e:
                 print(f"Failed on test result: {test_result}")
                 raise e
         # Test the whitelist is respected:
-        for test in [ 
-                (f"Jim Croce and {artist_whitelist[0]}", "a song title"), 
-                (f"Jim Croce & {artist_whitelist[0]}", "a song title"),
-                (f"Jim Croce with {artist_whitelist[0]}", "a song title"), 
-                (f"Jim Croce feat. {artist_whitelist[0]}", "a song title"), 
-                ]:
+        for test in [
+            (f"Jim Croce and {artist_whitelist[0]}", "a song title"),
+            (f"Jim Croce & {artist_whitelist[0]}", "a song title"),
+            (f"Jim Croce with {artist_whitelist[0]}", "a song title"),
+            (f"Jim Croce feat. {artist_whitelist[0]}", "a song title"),
+        ]:
             test_result = self.plugin.get_artists(test[0], test[1])
             try:
-                assert test_result == ['Jim Croce', {artist_whitelist[0]}] or [{artist_whitelist[0]}, 'Jim Croce']
+                assert test_result == ["Jim Croce", {artist_whitelist[0]}] or [
+                    {artist_whitelist[0]},
+                    "Jim Croce",
+                ]
             except AssertionError as e:
                 print(f"Failed on test result: {test_result}")
                 raise e
-        # Make sure song titles with the word "With" are not counted as an 
+        # Make sure song titles with the word "With" are not counted as an
         # artist, unless the word "with" is the first word in () or []
         for test_result in [
             self.plugin.get_artists(f"Taylor Swift", f"You Belong With Me"),
@@ -128,7 +189,9 @@ class AutoArtistsPluginTest(BeetsTestCase):
                 raise e
         # test that the word "with" inside () but not the first word will not be considered another artist
         for test_result in [
-            self.plugin.get_artists(f"Kate Bush", "Running Up That Hill (A Deal with God)"),
+            self.plugin.get_artists(
+                f"Kate Bush", "Running Up That Hill (A Deal with God)"
+            ),
         ]:
             try:
                 assert test_result == ["Kate Bush"]
@@ -137,7 +200,9 @@ class AutoArtistsPluginTest(BeetsTestCase):
                 raise e
         # Testing that "title (feat. X) (something else)" is interpreted correctly
         for test_result in [
-            self.plugin.get_artists(f"Taylor Swift", f"Breathe (feat. Colbie Caillat) (Taylor’s version)"),
+            self.plugin.get_artists(
+                f"Taylor Swift", f"Breathe (feat. Colbie Caillat) (Taylor’s version)"
+            ),
         ]:
             try:
                 assert test_result == ["Taylor Swift", "Colbie Caillat"]
@@ -162,14 +227,41 @@ class AutoArtistsPluginTest(BeetsTestCase):
                 print(f"Failed on test result: {test_result}")
                 raise e
         try:
-            assert len(self.plugin.get_artists("Taylor Swift", "Safe & Sound (feat. Joy Williams and John Paul White) (Taylor’s Version)", artists=['Taylor Swift', 'Joy Williams', ' John Paul White'])) == 3 
-            assert len(self.plugin.get_artists("Taylor Swift", "Safe & Sound (feat. Joy Williams and John Paul White) (Taylor’s Version)", artists=['Taylor Swift', 'Joy Williams', 'John Paul White', ' John Paul White'])) == 3 
+            assert (
+                len(
+                    self.plugin.get_artists(
+                        "Taylor Swift",
+                        "Safe & Sound (feat. Joy Williams and John Paul White) (Taylor’s Version)",
+                        artists=["Taylor Swift", "Joy Williams", " John Paul White"],
+                    )
+                )
+                == 3
+            )
+            assert (
+                len(
+                    self.plugin.get_artists(
+                        "Taylor Swift",
+                        "Safe & Sound (feat. Joy Williams and John Paul White) (Taylor’s Version)",
+                        artists=[
+                            "Taylor Swift",
+                            "Joy Williams",
+                            "John Paul White",
+                            " John Paul White",
+                        ],
+                    )
+                )
+                == 3
+            )
         except AssertionError as e:
             raise e
         for test_result in [
             self.plugin.get_artists(f"ZArtist A", f"Song Title (With Artist B)"),
-            self.plugin.get_artists(f"ZArtist A & Artist B", f"Song Title (With Artist B)"),
-            self.plugin.get_artists(f"ZArtist A Feat. Artist B", f"Song Title (With Artist B)"),
+            self.plugin.get_artists(
+                f"ZArtist A & Artist B", f"Song Title (With Artist B)"
+            ),
+            self.plugin.get_artists(
+                f"ZArtist A Feat. Artist B", f"Song Title (With Artist B)"
+            ),
         ]:
             try:
                 assert test_result == ["ZArtist A", "Artist B"]
@@ -177,15 +269,23 @@ class AutoArtistsPluginTest(BeetsTestCase):
                 print(f"Failed on test result: {test_result}")
                 raise e
         for test_result in [
-            self.plugin.get_artists(f"ZArtist A", f"Song Title (With Artist B and artist c)"),
-            self.plugin.get_artists(f"ZArtist A & Artist B, artist c", f"Song Title (With Artist B)"),
-            self.plugin.get_artists(f"ZArtist A Feat. Artist B & artist c", f"Song Title (With Artist B)"),
+            self.plugin.get_artists(
+                f"ZArtist A", f"Song Title (With Artist B and artist c)"
+            ),
+            self.plugin.get_artists(
+                f"ZArtist A & Artist B, artist c", f"Song Title (With Artist B)"
+            ),
+            self.plugin.get_artists(
+                f"ZArtist A Feat. Artist B & artist c", f"Song Title (With Artist B)"
+            ),
         ]:
             try:
                 assert test_result == ["ZArtist A", "Artist B", "artist c"]
             except AssertionError as e:
                 print(f"Failed on test result: {test_result}")
                 raise e
+
+
 test = AutoArtistsPluginTest()
 test.test_whitelist()
 test.test_separating_artist()
